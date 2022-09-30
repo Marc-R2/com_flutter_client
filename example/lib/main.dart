7298cf9dd589410b69e597d6d8e4f3cd71546b01
Marc Renken <marc.renken@bitbot.eu>
2022-09-30 13:37:50 +0200

    Introduce Beamer

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,29 +4,13 @@ import 'package:com_flutter_client/com_flutter_client.dart';
 
 void main() {
   // Message.info(title: 'App is starting');
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  runApp(COMApp(
+    homePage: MyHomePage(),
+  ));
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -77,31 +61,30 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Home'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            TextButton(
-              onPressed: () async {
-                final ms = ping.ping.request(
-                  fields: [ping.ping.wait],
-                  data: {'wait': 4096},
-                ).values;
-                await Future.wait(ms.toList());
-                setState(() => _counter = 0);
-              },
-              child: const Text('Reset (Test)'),
-            ),
-            const Expanded(child: InteractiveConsole()),
-          ],
-        ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text('You have pushed the button this many times:'),
+          Text(
+            '$_counter',
+            style: Theme.of(context).textTheme.headline4,
+          ),
+          TextButton(
+            onPressed: () async {
+              final ms = ping.ping.request(
+                fields: [ping.ping.wait],
+                data: {'wait': 4096},
+              ).values;
+              await Future.wait(ms.toList());
+              setState(() => _counter = 0);
+            },
+            child: const Text('Test - Connection to COM'),
+          ),
+          const Expanded(child: InteractiveConsole()),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
--- lib/com_flutter_client.dart
+++ lib/com_flutter_client.dart
@@ -1,10 +1,13 @@
 library com_flutter_client;
 
 export 'package:com_client/com_client.dart';
+export 'package:beamer/beamer.dart';
 
 import 'package:com_flutter_client/com_flutter_client.dart';
+import 'package:com_flutter_client/settings/settings.dart';
 import 'package:com_flutter_client/util/pill_select.dart';
 import 'package:flutter/material.dart';
+import 'package:beamer/beamer.dart';
 
 part 'console/log_console.dart';
 
@@ -14,4 +17,44 @@ part 'console/interactive_console.dart';
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+}
+
+class COMApp extends StatelessWidget {
+  COMApp({
+    super.key,
+    required this.homePage,
+    this.errorPage,
+  });
+
+  final Widget homePage;
+  final Widget? errorPage;
+
+  late final routerDelegate = BeamerDelegate(
+    initialPath: '/',
+    notFoundPage: BeamPage(
+      key: const ValueKey('404'),
+      child: errorPage ?? const Scaffold(
+        body: Center(
+          child: Text('404 - Not Found'),
+        ),
+      ),
+    ),
+    locationBuilder: RoutesLocationBuilder(
+      routes: {
+        '/': (context, state, data) => homePage,
+        '/settings': (context, state, data) => const Settings(),
+        '/settings/console': (context, state, data) => const InteractiveConsole(),
+        '/settings/console/log': (context, state, data) => const LogConsole(),
+        '/settings/console/log/:message': (context, state, data) => homePage,
+      },
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routeInformationParser: BeamerParser(),
+      routerDelegate: routerDelegate,
+    );
+  }
 }
